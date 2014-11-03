7e0da700deb280a4e74901fdb032a9c21634dddb
Nicolas Tessore <nicolas.tessore@unibo.it>
2014-11-03 17:01:34 +0000

    Make mock libmultinest from within runtests.jl.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
+# build the mock dynamic library
+run(`make`)
+
 # put the mock MultiNest library on DL_LOAD_PATH
-println("adding $(dirname(@__FILE__())) to DL_LOAD_PATH")
 push!(DL_LOAD_PATH, dirname(@__FILE__()))
 
 using MultiNest
@@ -7,3 +9,6 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+# done with tests, clean up
+run(`make clean`)
